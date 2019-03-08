867c81b42b02291fee1e270cc2f1f820891a21bc
tribalhelper <jota_p_santos@hotmail.com>
2019-03-08 14:25:52 +0000

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json');
 
-class Quiz extends React.Component {
+class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = {
