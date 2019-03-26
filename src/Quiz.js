2b7b99b1086f319254737a93d23b62fa27d73def
tribalhelper <jota_p_santos@hotmail.com>
2019-03-26 14:26:53 +0000

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,9 +12,13 @@ class Quiz extends Component {
     }
 
     render() {
+        const isQuizEnd = ((this.state.quiz_position -1 ) === quizData.quiz_questions.length);
         return (
             <div>
-                {(this.state.quiz_position - 1) == quizData.quiz_questions.length ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}                
+            {isQuizEnd ?
+                <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1] } />  
+            }          
             </div>
         );
     }
